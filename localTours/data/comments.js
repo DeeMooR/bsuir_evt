cbb08362d1c7edfd3267047788c03d2d8c65d3f9
Dmitry Matyshev <deemoor4@gmail.com>
2025-03-03 14:16:27 +0300

    feat: Footer, slider

---
+++ localTours/data/comments.js
@@ -0,0 +1,38 @@
+export const comments = [
+  {
+    id: 1,
+    image: '../assets/images/userPhoto_1.png',
+    text: 'Comment 1 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - Designer'
+  },
+  {
+    id: 2,
+    image: '../assets/images/userPhoto_2.png',
+    text: 'Comment 2 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - Frontend'
+  },
+  {
+    id: 3,
+    image: '../assets/images/userPhoto_1.png',
+    text: 'Comment 3 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - Backend'
+  },
+  {
+    id: 4,
+    image: '../assets/images/userPhoto_2.png',
+    text: 'Comment 4 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - PM'
+  },
+  {
+    id: 5,
+    image: '../assets/images/userPhoto_1.png',
+    text: 'Comment 5 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - BA'
+  },
+  {
+    id: 6,
+    image: '../assets/images/userPhoto_2.png',
+    text: 'Comment 6 officiis dolor ea illo aut eligendi ullam non laudantium magnam et recusandae molestiae sit iure unde aut voluptate quaerat. Id sunt provident quo possimus impedit vel doloremque obcaecati qui ullam consectetur et ipsum omnis.',
+    author: 'Christine Beckam - Frontend Lead'
+  }
+]
---
+++ localTours/scripts/comments.js
@@ -0,0 +1,41 @@
+import { comments } from '../data/comments.js'
+
+const btnLeft = document.querySelector('.card__btnLeft');
+const btnRight = document.querySelector('.card__btnRight');
+
+const cardImage = document.querySelector('.card__image');
+const cardText = document.querySelector('.card__text');
+const cardAuthor = document.querySelector('.card__author');
+
+let activeComment = 1;
+
+const updateActivePagination = (oldId, obj) => {
+  const oldElement = document.querySelector(`#pagination_${oldId}`);
+  const newElement = document.querySelector(`#pagination_${obj.id}`);
+  oldElement.classList.remove('pagination__active');
+  newElement.classList.add('pagination__active');
+}
+
+btnRight.addEventListener('click', () => {
+  const oldId = activeComment;
+  if (activeComment === comments.length) activeComment = 1;
+  else activeComment += 1;
+
+  const obj = comments.find(item => item.id === activeComment);
+  cardImage.src = obj.image;
+  cardText.textContent = obj.text;
+  cardAuthor.textContent = obj.author;
+  updateActivePagination(oldId, obj);
+})
+
+btnLeft.addEventListener('click', () => {
+  const oldId = activeComment;
+  if (activeComment === 1) activeComment = comments.length;
+  else activeComment -= 1;
+
+  const obj = comments.find(item => item.id === activeComment);
+  cardImage.src = obj.image;
+  cardText.textContent = obj.text;
+  cardAuthor.textContent = obj.author;
+  updateActivePagination(oldId, obj);
+})
